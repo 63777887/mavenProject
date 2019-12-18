22ec968cd62e78e51120e42ac5fa11c0458a402f
63777887@qq.com <63777887@qq.com>
2019-12-18 20:03:04 +0800

    src

--- src/main/java/understandMaven/example/CsvTest.java
+++ src/main/java/understandMaven/example/CsvTest.java
@@ -1,7 +1,30 @@
 package understandMaven.example;
 
-public class CsvTest {
-    public static void main(String[] args) {
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class CsvTest {
+    public static void main(String[] args) throws IOException {
+        CsvWriter csvWriter=new CsvWriter("test.csv", '.', Charset.forName("utf-8"));
+        String []str={"学生编号","学生姓名","学生年龄","学生住址"};
+        //具体的内容信息
+        String [] comment1={"9527","王五1","21","垃圾桶"};
+        String [] comment2={"9528","王五2","21","垃圾桶"};
+        String [] comment3={"9529","王五3","21","垃圾桶"};
+
+        csvWriter.writeRecord(str);
+        csvWriter.writeRecord(comment1);
+        csvWriter.writeRecord(comment2);
+        csvWriter.writeRecord(comment3);
+        csvWriter.close();
+
+        CsvReader csvReader=new CsvReader("test.csv",'.',Charset.forName("utf-8"));
+        csvReader.readHeaders();
+        while (csvReader.readRecord()){
+            System.out.println(csvReader.getRawRecord());
+        }
     }
 }
--- src/main/java/understandMaven/example/Dom4jTest.java
+++ src/main/java/understandMaven/example/Dom4jTest.java
@@ -1,7 +1,26 @@
 package understandMaven.example;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.util.Iterator;
+
 public class Dom4jTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DocumentException {
+        SAXReader saxReader=new SAXReader();
+        Document document=saxReader.read("student.xml");
+        Element element=document.getRootElement();
+        Iterator iterator=element.elementIterator();
+        while (iterator.hasNext()) {
+            Element et = (Element) iterator.next();
+            Iterator it=et.elementIterator();
+            while (it.hasNext()){
+                Element e=(Element)it.next();
+                System.out.println(e.getName()+"---"+e.getStringValue());
+            }
+        }
 
     }
 }
